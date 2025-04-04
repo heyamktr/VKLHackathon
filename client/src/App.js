d3a829f419c6472be8c784bae4112b151c0f6d34
Truong Nguyen Tien Khanh <88968349+khanhtruongloveu@users.noreply.github.com>
2025-04-04 09:15:38 -0400

    Create App.js

---
+++ client/src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Header from "./components/Header";
+import PreferenceForm from "./components/PreferenceForm";
+import ChatBox from "./components/ChatBox";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <PreferenceForm />
+      <ChatBox />
+    </div>
+  );
+}
+
+export default App;
